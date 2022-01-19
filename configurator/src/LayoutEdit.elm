280813a3032c244f88f9c30b8456d45b36923d54
Ben Burdette <bburdette@gmail.com>
2022-01-18 20:24:53 -0700

    sizer buttons

--- configurator/src/LayoutEdit.elm
+++ configurator/src/LayoutEdit.elm
@@ -34,6 +34,8 @@ type Msg
     = AddButtonPress
     | AddHSliderPress
     | AddVSliderPress
+    | AddHSizerPress
+    | AddVSizerPress
     | AddXYPress
     | ScpMsg SvgControlPage.Msg
 
@@ -69,6 +71,14 @@ view size model =
     E.row [ E.width E.fill ]
         [ E.column [ E.spacing 8, E.width E.fill ]
             [ EI.button buttonStyle
+                { onPress = Just AddHSizerPress
+                , label = E.text "AddHSizer"
+                }
+            , EI.button buttonStyle
+                { onPress = Just AddVSizerPress
+                , label = E.text "AddVSizer"
+                }
+            , EI.button buttonStyle
                 { onPress = Just AddButtonPress
                 , label = E.text "AddButton"
                 }
@@ -78,19 +88,26 @@ view size model =
                 }
             , EI.button buttonStyle
                 { onPress = Just AddVSliderPress
-                , label = E.text "AddVSlider..."
+                , label = E.text "AddVSlider"
                 }
             , EI.button buttonStyle
                 { onPress = Just AddXYPress
-                , label = E.text "AddXY..."
+                , label = E.text "AddXY"
                 }
             ]
+        , E.map ScpMsg <| E.html (SvgControlPage.view model.scpModel)
         ]
 
 
 update : Msg -> Model -> ( Model, Command )
 update msg model =
     case msg of
+        AddHSizerPress ->
+            ( model, None )
+
+        AddVSizerPress ->
+            ( model, None )
+
         AddButtonPress ->
             ( model, None )
 
